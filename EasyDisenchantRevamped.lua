ef8b2cee1ffbd67fcc001cfd2a208f65ce769450
Miki Urban <mikiurban@hotmail.com>
2025-12-09 22:54:41 -0800

    Fixes to Patch 11.2.7

--- EasyDisenchantRevamped.lua
+++ EasyDisenchantRevamped.lua
@@ -302,7 +302,7 @@ do
 			buttons[i]:Hide();
 		end
 
-		local disenchantName = GetSpellInfo(13262);		
+		local disenchantName = C_Spell.GetSpellInfo(13262).name;
 		local macroFormat = "/stopmacro [combat][btn:2]\n/stopcasting\n/cast %s\n/cast %s %s";	
 		self:ResetEquipmentManagerCache();
 
@@ -313,16 +313,15 @@ do
 	
 				-- Skip non-existant items or legendary+.
 				if item ~= nul and item.hyperlink ~= nil and (item.quality ~= nil and item.quality < 5 and item.quality > 1) then
-					local itemName, _, _, _, _, itemClass, itemSubClass = GetItemInfo(item.hyperlink);
+					local itemID, itemType, itemSubType = C_Item.GetItemInfoInstant(item.hyperlink);
 
 					-- Avoid breaking on M+ keys
-					if itemSubClass ~= nil then
+					if itemSubType ~= nil then
 						-- Check Blacklist
-						local itemID = item.itemID;
 						
 						if not self:IsBlacklisted(itemID) and self:IsItemInOutfit(bagID, slotID, itemID) == false then
 							-- Only disenchant weapons and armour.
-							if itemClass == WEAPON or itemClass == ARMOR or itemSubClass:find(ITEM_QUALITY6_DESC) then
+							if itemType == WEAPON or itemType == ARMOR then
 								local button = self:GetItemButton(useButton);
 							
 								SetItemButtonTexture(button, item.iconFileID);
@@ -334,8 +333,6 @@ do
 						
 								button.link = item.hyperlink;
 								button.itemID = itemID;
-								-- button.bagID = bagID;
-								-- button.slotID = slotID;
 								
 								button:Show();
 								if useButton == self.maxButtons then
@@ -543,7 +540,7 @@ do
 		self.blacklist = EasyDisenchantBlacklist;
 
 		-- Hook to TradeSkillFrame.
-		if not self.isTradeSkillFrameHooked and IsAddOnLoaded("Blizzard_TradeSkillUI") then
+		if not self.isTradeSkillFrameHooked and C_AddOns.IsAddOnLoaded("Blizzard_TradeSkillUI") then
 			self:HookTradeSkillFrame();
 		end
 	end
